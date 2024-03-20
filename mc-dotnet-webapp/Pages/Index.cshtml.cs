51d7b3665d653ddf31297f7a383ca85ef3c7d85e
markcaiOH <mark.cai@ontariohealth.ca>
2024-03-20 09:50:23 -0400

    Added call to HAPI FHIR server

--- mc-dotnet-webapp/Pages/Index.cshtml.cs
+++ mc-dotnet-webapp/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Text.Json;
 
 namespace mc_dotnet_webapp.Pages
 {
@@ -10,6 +11,7 @@ namespace mc_dotnet_webapp.Pages
         private readonly IHttpClientFactory _httpClientFactory;
         public string GUID { get; private set; }
         public string ResponseData { get; private set; }
+        public string MorePatientDetails { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
         {
@@ -28,19 +30,43 @@ namespace mc_dotnet_webapp.Pages
                 if (response.IsSuccessStatusCode)
                 {
                     ResponseData = await response.Content.ReadAsStringAsync();
+                    var patientData = JsonSerializer.Deserialize<PatientData>(ResponseData);
+                    var patientId = patientData?.Id;
+
+                    MorePatientDetails = await FetchFromFhirAsync(patientId);
                 } else
                 {
                     ResponseData = "Error retrieving data.";
                 }
             }
         }
-        
-        /*
-        public void OnGet(string guid)
-        {
-            GUID = guid;
-        }
-        */
 
+        public async Task<string> FetchFromFhirAsync(string patientId)
+        {
+            if (!String.IsNullOrWhiteSpace(patientId))
+            {
+                var client = _httpClientFactory.CreateClient();
+                var fhirResponse = await client.GetAsync($"https://hapi.fhir.org/baseR4/Patient/{patientId}");
+                if (fhirResponse.IsSuccessStatusCode)
+                {
+                    return await fhirResponse.Content.ReadAsStringAsync();
+                } 
+                else
+                {
+                    _logger.LogError($"Failed to retrieve more patient details from FHIR for Patient ID: {patientId}");
+                    return "Error retrieving more patient details from FHIR.";
+                }
+            }
+            return "No Patient ID provided.";
+        }
+
+        private class PatientData
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string HealthCardNumber { get; set; }
+        }
     }
 }
